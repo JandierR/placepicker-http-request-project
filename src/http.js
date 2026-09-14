4fec6723c581af245c2efc8c39edfb6d0ede9dd5
Jandier Rojas Zúñiga <jandier.rojas.z@gmail.com>
2026-09-14 10:43:24 -0600

    Add post http rquest

--- src/http.js
+++ src/http.js
@@ -8,3 +8,19 @@ export async function fetchAvailablePlaces() {
 
   return resData.places;
 }
+
+export async function updateUserPlaces(places) {
+  const response = await fetch('http://localhost:3000/user-places', {
+    method: 'PUT',
+    body: JSON.stringify({ places: places }),
+    headers: { 'Content-type': 'application/json' },
+  });
+
+  const resData = response.json();
+
+  if (!response.ok) {
+    throw new Error('Failed to update user data.');
+  }
+
+  return resData.message;
+}
